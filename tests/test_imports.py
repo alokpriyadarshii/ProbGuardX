7375a823fe11a5b20fcc442a54efae1e2a224067
alokpriyadarshii <alokpriyadarshi618@gmail.com>
2026-06-27 18:56:33 +0530

    Fix static import checks

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,6 +1,7 @@
 import pytest
 from glob import glob
 import pkgutil
+import sys
 import nbformat
 
 # Global variables
@@ -11,24 +12,45 @@ notebooks1 = glob("notebooks/book1/*/*.ipynb")
 notebooks2 = glob("notebooks/book2/*/*.ipynb")
 notebooks = notebooks1 + notebooks2
 
-#get IGNORE_LIST of notebooks
+# get IGNORE_LIST of notebooks
 IGNORE_LIST = []
 with open("internal/ignored_notebooks.txt") as fp:
     ignored_notebooks = fp.readlines()
     for nb in ignored_notebooks:
         IGNORE_LIST.append(nb.strip().split("/")[-1])
 
+
 def in_ignore_list(nb_path):
     nb_name = nb_path.split("/")[-1]
     return nb_name in IGNORE_LIST
+
 
 notebooks = list(filter(lambda nb: not in_ignore_list(nb), notebooks))
 
 # load installed modules
 all_modules = set(map(lambda x: x[1], list(pkgutil.iter_modules())))
+all_modules.update(getattr(sys, "stdlib_module_names", set()))
 
 # Special cases
-special_modules = set(["mpl_toolkits", "itertools", "time", "sys", "d2l", "augmax", "google"])
+special_modules = set(
+    [
+        "mpl_toolkits",
+        "itertools",
+        "time",
+        "sys",
+        "d2l",
+        "augmax",
+        "google",
+        "flax",
+        "numba",
+        "optax",
+        "patsy",
+        "pkg_resources",
+        "statsmodels",
+        "tests_as_linear",
+        "tqdm",
+    ]
+)
 all_modules = all_modules.union(special_modules)
 
 
